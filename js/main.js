545f532d9612ad3ba9f248767f21a03ad1a8b0c3
Igor Lavskij <i.lavskij@gmail.com>
2017-06-12 19:47:14 +0300

    descktop version of the site

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    var mySwiper = new Swiper('.swiper-container', {
+        speed: 400,
+        loop: true,
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        nextButton: '.btn-next',
+        prevButton: '.btn-prev',
+    });
+
+    $('.header-bottom').affix({offset: {top: 182}});
+});
